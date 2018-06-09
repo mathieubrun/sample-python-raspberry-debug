bb15a7f70df2cf4f0193ab4ab743ea3b9266aa42
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2018-06-09 21:55:36 +0200

    Connect using keys

--- debug-pi.sh
+++ debug-pi.sh
@@ -5,7 +5,8 @@ USER=pi
 
 docker run --rm -ti \
         --workdir /__work \
+        -v "${HOME}/.ssh:/root/.ssh" \
         -v "$(pwd):/__work" \
-        mathieubrun/rsync:latest -ratlz ssh --progress . ${USER}@${HOST}:/home/pi/test
+        mathieubrun/rsync:latest -ratlz --progress . ${USER}@${HOST}:/home/pi/test
 
 ssh ${USER}@${HOST} -t 'pushd /home/pi/test && ./debug-local.sh'
